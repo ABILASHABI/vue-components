44f866f84eb5cb0fb71987cd4dc61aae21d00189
ABILASHABI <87457958+ABILASHABI@users.noreply.github.com>
2021-09-05 12:50:32 +0530

    Add files via upload

---
+++ src/components/Child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center justify-center">
+    <h2
+      class="text-3xl 
+    h-32 border-green-800 w-1/2 text-green-800 shadow-outline"
+    >
+      
+      {{ message }}
+      <span>Child</span>
+      <div class="text-2xl italic font-bold">Received: {{ message }}</div>
+    </h2>
+  </div>
+</template>
+  
+<script>
+
+
+import { mapGetters } from "vuex";
+export default {
+  
+  computed: {
+    ...mapGetters({ message: "getMessage" })
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1 class="bg-">lets enjoy vue with the following commands</h1><br>
+     <h1 class="bg-"><b>(/=> parent to child communication)</b></h1><br>
+      <h1 class="bg-"><b>(/weather=> Awesome weather updates)</b></h1><br>
+       <h1 class="bg-"><b>(/calculator=> math calculations)</b></h1><br>
+  </div>
+  </template>
+  
